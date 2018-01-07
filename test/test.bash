7ac72bb3ed9a93aa6f910d98d8fe631ea4e3d712
beardhatcode <mail.garonn@gmail.com>
2018-01-07 20:38:43 +0100

    fix(spec): We should have a workdir

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,10 @@
 #!/bin/bash
 DIR="$(dirname $0 | xargs readlink -m)"
-rm -rf $DIR/workdir/* 2> /dev/null
-cp -r $DIR/ex/description/media/* $DIR/workdir 2> /dev/null
 $DIR/../run <<HERE
 {
     "resources": "$DIR/ex/evaluation",
     "judge": "$DIR/..",
-    "workdir": "$DIR/workdir",
+    "workdir": "$DIR/ex/workdir",
     "time_limit": 30,
     "memory_limit": 100000000,
     "source": "$DIR/ex/solution/input.pl",
